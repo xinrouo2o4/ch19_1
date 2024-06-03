774ba3dd7c83d7a229b8cc843209a0249502015b
Hsin Jou Chen <C111156121@nkust.edu.tw>
2024-06-03 14:52:59 +0800

    vercel

--- src/App.js
+++ src/App.js
@@ -4,20 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>C111156121 陳歆媃</h1>
+      <a>網頁部屬測試！</a>
     </div>
   );
 }
